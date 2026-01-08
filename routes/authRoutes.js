061c8ec6cec8e8f440303a13d09a50b487c57c37
haroonharish <haroonharish9@gmail.com>
2026-01-08 14:31:43 +0530

    Restrict agent registration to admin-only access

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const authController = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post("/register", authController.register);
+router.post("/admin/agent/register",authMiddleware(["ADMIN"]), authController.register);
 router.post("/login", authController.login);
 router.post("/change-password", authMiddleware(["AGENT", "ADMIN"]), authController.changePassword);
 module.exports = router;
