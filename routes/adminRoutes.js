19cfd3f954677649f3d351ffec3243339734edfa
haroonharish <haroonharish9@gmail.com>
2026-02-22 16:22:19 +0530

    Declare generateMonthlyReport variable

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const upload = require("../middleware/upload");
 const auth = require("../middleware/authMiddleware");
-const { uploadCustomers, getCustomerReports, getAgentsSummary, getAgentCustomers, getExcelUploadHistory } = require("../controllers/adminController");
+const { uploadCustomers, getCustomerReports, getAgentsSummary, getAgentCustomers, getExcelUploadHistory, generateMonthlyReport } = require("../controllers/adminController");
 const { resetAgentPassword } = require("../controllers/authController");
 
 router.post("/upload-customers", auth(["ADMIN"]), upload.single("file"), uploadCustomers);
