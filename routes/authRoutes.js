304d4e5725282c48b61a3502de99043915f5665e
haroonharish <haroonharish9@gmail.com>
2026-03-02 18:21:16 +0530

    update endpoint for reset agent password2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/admin/agent/register",authMiddleware(["ADMIN"]), authController.register);
 router.post("/login", authController.login);
-router.post("/reset-agent-password", authMiddleware(["ADMIN"]), authControllerresetAgentPassword);
+router.post("/reset-agent-password", authMiddleware(["ADMIN"]), authController.resetAgentPassword);
 router.post("/change-password", authMiddleware(["AGENT", "ADMIN"]), authController.changePassword);
 router.post("/admin/agent/delete",authMiddleware(["ADMIN"]), authController.deleteAgent);
 router.post("/admin/agent/restore",authMiddleware(["ADMIN"]), authController.restoreAgent);
