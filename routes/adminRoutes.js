21c51a8264f9f434fc843379f622f96ff30ec234
haroonharish <haroonharish9@gmail.com>
2026-03-02 17:58:00 +0530

    update endpoint for reset agent password

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,11 +2,9 @@ const router = require("express").Router();
 const upload = require("../middleware/upload");
 const auth = require("../middleware/authMiddleware");
 const { uploadCustomers, getCustomerReports, getAgentsSummary, getAgentCustomers, getExcelUploadHistory, generateBatchReport } = require("../controllers/adminController");
-const { resetAgentPassword } = require("../controllers/authController");
 
 router.post("/upload-customers", auth(["ADMIN"]), upload.single("file"), uploadCustomers);
 router.get("/customer-reports", auth(["ADMIN"]), getCustomerReports);
-router.post("/reset-agent-password", auth(["ADMIN"]), resetAgentPassword);
 router.get("/agent-summary",auth(["ADMIN"]), getAgentsSummary);
 router.get("/agent/:agentCustomId/Customers",auth(["ADMIN"]), getAgentCustomers);
 router.get("/upload-history", auth(["ADMIN"]), getExcelUploadHistory);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post("/admin/agent/register",authMiddleware(["ADMIN"]), authController.register);
 router.post("/login", authController.login);
+router.post("/reset-agent-password", authMiddleware(["ADMIN"]), authControllerresetAgentPassword);
 router.post("/change-password", authMiddleware(["AGENT", "ADMIN"]), authController.changePassword);
 router.post("/admin/agent/delete",authMiddleware(["ADMIN"]), authController.deleteAgent);
 router.post("/admin/agent/restore",authMiddleware(["ADMIN"]), authController.restoreAgent);
