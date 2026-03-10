52abfd3aabfaa4c4b8c362db7f9475528a499f4a
haroonharish <haroonharish9@gmail.com>
2026-03-10 14:52:31 +0530

    debug getAttendance

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const upload = require("../middleware/upload");
 const auth = require("../middleware/authMiddleware");
-const { uploadCustomers, getCustomerReports, getAgentsSummary, getAgentCustomers, getExcelUploadHistory, deleteUpload, generateBatchReport } = require("../controllers/adminController");
+const { uploadCustomers, getCustomerReports, getAgentsSummary, getAgentCustomers, getExcelUploadHistory, deleteUpload, generateBatchReport,getAttendance } = require("../controllers/adminController");
 
 router.post("/upload-customers", auth(["ADMIN"]), upload.single("file"), uploadCustomers);
 router.get("/customer-reports", auth(["ADMIN"]), getCustomerReports);
